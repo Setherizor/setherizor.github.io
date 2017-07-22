e746c6ceafbf4ba0cdf3648c52f1c9c2fbd4b748
Setherizor <setherizor@gmail.com>
2017-07-22 17:12:26 -0400

    Fixed the multiple cards issue

--- js/requests.js
+++ js/requests.js
@@ -5,7 +5,7 @@ PROJECT_LIST_ID = "59722a00877dfde4b1feaa70";
 PROJECT_CARDS = [];
 PROJECT_URLS = [];
 ID = 0;
-
+ID_COUNTER= 0;
 
 function putOnPage(p) {
     var list = document.getElementById("TheList");
@@ -13,43 +13,22 @@ function putOnPage(p) {
         var curr = p[i];
         var attach = '<div class="section dark">' +
             "<h3>" + curr.name + "</h3>" +
-            "<p>" + curr.desc + "</p>" +
-            "<p><a href='" + PROJECT_URLS[i] + "' rel='noopener'>" + PROJECT_URLS[i] + "</a>" + "<br>" +
-            "Last Modified: " + curr.dateLastActivity.substring(0, 10) + "</p></div>";
+            "<p>" + curr.desc + "</p>";
+
+        for (var j = 0; j < PROJECT_URLS[i].length; j++) {
+            attach += "<p><a href='" + PROJECT_URLS[i][j].url + "' rel='noopener'>" + PROJECT_URLS[i][j].url + "</a>" + "<br>";
+        }
+
+        attach += "Last Modified: " + curr.dateLastActivity.substring(0, 10) + "</p></div>";
+
         if (i != p.length - 1) attach += "<br>";
         list.innerHTML += attach;
     }
 }
-
-function loadJSONFile(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'projects.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            console.log(xobj.responseText);
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-var getEm = function () {
-    loadJSONFile(function (response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        var p = actual_JSON;
-        console.log("projects got and bout sent")
-        putOnPage(p);
-    });
-}
-//getEm();
-
-
-// NEW JUNk
-
 function trelloJSON(callback) {
-    var url = "https://api.trello.com/1/boards/" + BOARD_ID + "/cards?fields=name,url,idList,desc,dateLastActivity,?&key=" + TRELLO_API + "&token=" + TRELLO_TOKEN;
+    var url = "https://api.trello.com/1/boards/" + BOARD_ID +
+        "/cards?fields=name,url,idList,desc,dateLastActivity,?&key=" +
+        TRELLO_API + "&token=" + TRELLO_TOKEN;
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -57,7 +36,7 @@ function trelloJSON(callback) {
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            console.log(xobj.responseText);
+            // console.log(xobj.responseText);
             callback(xobj.responseText);
         }
     };
@@ -65,7 +44,9 @@ function trelloJSON(callback) {
 }
 var getAttachments = function (callback) {
     var cardId = PROJECT_CARDS[ID].id;
-    var url = "https://api.trello.com/1/cards/" + cardId + "/attachments?&fields=url,&key=" + TRELLO_API + "&token=" + TRELLO_TOKEN;
+    var url = "https://api.trello.com/1/cards/" +
+        cardId + "/attachments?&fields=url,&key=" +
+        TRELLO_API + "&token=" + TRELLO_TOKEN;
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -73,7 +54,7 @@ var getAttachments = function (callback) {
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            //console.log(xobj.responseText);
+            // console.log(xobj.responseText);
             callback(xobj.responseText);
         }
     };
@@ -84,24 +65,31 @@ var useThem = function () {
         ID = i;
         getAttachments(function (response) {
             var actual_JSON = JSON.parse(response);
-
-            PROJECT_URLS.push(actual_JSON[0].url);
-                
-            console.log(actual_JSON[0].url);
-
+            PROJECT_URLS[ID_COUNTER] = actual_JSON;
+            ID_COUNTER++;
             if (PROJECT_URLS.length === PROJECT_CARDS.length) {
                 putOnPage(PROJECT_CARDS);
             }
         });
     }
 }
+var checkThem = function (p) {
+    for (var i = 0; i < p.length; i++) {
+        curr = p[i];
+        if (curr.idList == PROJECT_LIST_ID) {
+            PROJECT_CARDS.push(curr);
+            console.log("Added " + curr.name);
+        } else {
+            // Do nothing
+            // console.log("Ignored " + curr.name);
+        }
+    }
+}
 var getThem = function () {
     trelloJSON(function (response) {
-        // Parse JSON string into object
         var actual_JSON = JSON.parse(response);
-        var p = actual_JSON;
-        console.log(p);
-        PROJECT_CARDS = p;
+        // console.log(actual_JSON);
+        checkThem(actual_JSON);
         useThem();
     });
 }
