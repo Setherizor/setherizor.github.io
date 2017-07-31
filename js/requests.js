ffb4eae1b18919b5bd8a0027f67c8eaaa35bb97d
Setherizor <setherizor@gmail.com>
2017-07-31 11:34:00 -0400

    Completely redesigned the request.js file. Works mush faster

--- js/requests.js
+++ js/requests.js
@@ -2,78 +2,66 @@ const TRELLO_API = 'cda33ce6abce02250524b0a019e111a7'
 const TRELLO_TOKEN = '9030fe39125e16ef5cda171ac179edcc59c768c0b930cf76d369a4ea2797cd77'
 const BOARD_ID = 'JwRH1hMD'
 const PROJECT_LIST_ID = '59722a00877dfde4b1feaa70'
-var PROJECT_CARDS = []
-const PROJECT_URLS = []
 var ID = 0
-var ID_COUNTER = 0
 
-function putOnPage(p) {
+function putOnPage (cards) {
   var list = document.getElementById('TheList')
-  for (var i = 0; i < p.length; i++) {
-    var curr = p[i]
+  for (var i = 0; i < cards.length; i++) {
+    var curr = cards[i]
     var attach = `<div class="section dark"><h3>${curr.name}</h3><p>${curr.desc}</p>`
-    for (var j = 0; j < PROJECT_URLS[i].length; j++) {
-      attach += `<p><a href="${PROJECT_URLS[i][j].url}" rel='noopener'>${PROJECT_URLS[i][j].url}</a><br>`
+    for (var j = 0; j < curr.urls.length; j++) {
+      attach += `<p><a href="${curr.urls[j]}" rel='noopener'>${curr.urls[j]}</a><br>`
     }
-    attach += `Last Modified:${curr.dateLastActivity.substring(0, 10)}</p></div>`
-    if (i !== p.length - 1) attach += '<br>'
+    attach += `Last Modified: ${curr.dateLastActivity.substring(0, 10)}</p></div>`
+    if (i !== cards.length - 1) attach += '<br>'
     list.innerHTML += attach
   }
 }
-function trelloJSON(callback) {
+function generalRequest (callback, url) {
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', url, true)
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText)
+    }
+  }
+  xobj.send(null)
+}
+function trelloJSON (callback) {
   var url = `https://api.trello.com/1/boards/${BOARD_ID}/cards?fields=name,url,idList,desc,dateLastActivity,?&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
-  var xobj = new XMLHttpRequest()
-  xobj.overrideMimeType('application/json')
-  xobj.open('GET', url, true)
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState === 4 && xobj.status === 200) {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      callback(xobj.responseText)
-    }
-  }
-  xobj.send(null)
+  generalRequest(function (response) {
+    callback(response)
+  }, url)
 }
-var getAttachments = function (callback) {
-  var cardId = PROJECT_CARDS[ID].id
+var getAttachments = function (callback, cards) {
+  var cardId = cards[ID].id
+  ID++
   var url = `https://api.trello.com/1/cards/${cardId}/attachments?&fields=url,&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
-  var xobj = new XMLHttpRequest()
-  xobj.overrideMimeType('application/json')
-  xobj.open('GET', url, true)
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState === 4 && xobj.status === 200) {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      // console.log(xobj.responseText);
-      callback(xobj.responseText)
-    }
-  }
-  xobj.send(null)
+
+  generalRequest(function (response) {
+    callback(response)
+  }, url)
 }
-var useThem = function () {
-  var readyToPrint = function () {
-    if (PROJECT_URLS.length === PROJECT_CARDS.length) {
-      putOnPage(PROJECT_CARDS)
-    }
-  }
-  for (var i = 0; i < PROJECT_CARDS.length; i++) {
-    ID = i
+var useThem = function (cards) {
+  cards.map(function (card, index) {
     getAttachments(function (response) {
       var actual = JSON.parse(response)
-      PROJECT_URLS[ID_COUNTER] = actual
-      ID_COUNTER++
-      readyToPrint()
-    })
-  }
-}
-var checkThem = function (p) {
-  PROJECT_CARDS = p.filter(function (card) {
-    return card.idList === PROJECT_LIST_ID
+      cards[index].urls = actual.map(function (attachment) {
+        return attachment.url
+      })
+      putOnPage([cards[index]])
+    }, cards)
   })
 }
 var getThem = function () {
   trelloJSON(function (response) {
-    var actual = JSON.parse(response)
-    checkThem(actual)
-    useThem()
+    var allJson = JSON.parse(response)
+    var cards = allJson.filter(function (card) {
+      return card.idList === PROJECT_LIST_ID
+    })
+    useThem(cards)
   })
 }
 getThem()
