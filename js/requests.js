4adc690dec3303fb145562fce674e73271a6ea0c
Setherizor <setherizor@gmail.com>
2017-08-04 20:54:16 -0400

    Removed all vars, implemented more functional programming concepts

--- js/requests.js
+++ js/requests.js
@@ -1,58 +1,66 @@
 const TRELLO_API = 'cda33ce6abce02250524b0a019e111a7'
 const TRELLO_TOKEN = '9030fe39125e16ef5cda171ac179edcc59c768c0b930cf76d369a4ea2797cd77'
-const BOARD_ID = 'JwRH1hMD'
 const PROJECT_LIST_ID = '59722a00877dfde4b1feaa70'
-var ID = 0
+const BOARD_ID = 'JwRH1hMD'
 
-function putOnPage (cards) {
-  var list = document.getElementById('TheList')
-  for (var i = 0; i < cards.length; i++) {
-    var curr = cards[i]
-    var attach = `<div class="section dark"><h3>${curr.name}</h3><p>${curr.desc}</p>`
-    for (var j = 0; j < curr.urls.length; j++) {
-      attach += `<p><a href="${curr.urls[j]}" rel='noopener'>${curr.urls[j]}</a><br>`
-    }
-    attach += `Last Modified: ${curr.dateLastActivity.substring(0, 10)}</p></div>`
-    if (i !== cards.length - 1) attach += '<br>'
-    list.innerHTML += attach
-  }
-}
-function generalRequest (callback, url) {
+/**
+ * Performs a General HTTP request which calls callback with JSON
+ * @param {Function} callback 
+ * @param {String} url 
+ */
+const generalRequest = function (callback, url) {
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => callback(data))
 }
-function trelloJSON (callback) {
-  var url = `https://api.trello.com/1/boards/${BOARD_ID}/cards?fields=name,url,idList,desc,dateLastActivity,?&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
-  generalRequest(function (response) {
-    callback(response)
-  }, url)
-}
-var getAttachments = function (callback, cards) {
-  var cardId = cards[ID].id
-  ID++
-  var url = `https://api.trello.com/1/cards/${cardId}/attachments?&fields=url,&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
-
-  generalRequest(function (response) {
-    callback(response)
-  }, url)
-}
-var useThem = function (cards) {
-  cards.map(function (card, index) {
-    getAttachments(function (response) {
-      cards[index].urls = response.map(function (attachment) {
-        return attachment.url
-      })
-      putOnPage([cards[index]])
-    }, cards)
+/**
+ * Takes a card, and attached its dats in a HTML string to "TheList"
+ * @param {Card} card 
+ */
+const putOnPage = function (card) {
+  const list = document.getElementById('TheList')
+  let attach = `<div class="section dark"><h3>${card.name}</h3><p>${card.desc}</p>`
+  card.urls.map((url) => {
+    attach += `<p><a href="${url}" rel='noopener'>${url}</a><br>`
   })
+  attach += `Last Modified: ${card.dateLastActivity.substring(0, 10)}</p></div>`
+  attach += '<br>'
+
+  list.innerHTML += attach
 }
-var getThem = function () {
-  trelloJSON(function (response) {
-    var cards = response.filter(function (card) {
+/**
+ * Makes request to Trello asking for a given card's attachments
+ * HAVE NOT TESTES WITH NON URL ATTACHMENTS
+ * @param {Card} card 
+ */
+const getAttachments = function (card) {
+  const url = `https://api.trello.com/1/cards/${card.id}/attachments?&fields=url,&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
+  const getUrls = x => x.map((attachment) => attachment.url)
+  generalRequest((response) => {
+    card.urls = getUrls(response)
+    putOnPage(card)
+  }, url)
+}
+/**
+ * Gets attachments for each card in cards
+ * also end up appending urls to the card object
+ * @param {Array} cards 
+ */
+const useThem = (cards) => {
+  cards.map((card) => getAttachments(card))
+}
+/**
+ * Makes request to TRELLO for all cards,
+ * filters out all not in desired list,
+ * then calls useThem with said cards
+ */
+const trelloJSONImport = function () {
+  const url = `https://api.trello.com/1/boards/${BOARD_ID}/cards?fields=name,url,idList,desc,dateLastActivity,?&key=${TRELLO_API}&token=${TRELLO_TOKEN}`
+  generalRequest((response) => {
+    const cards = response.filter(function (card) {
       return card.idList === PROJECT_LIST_ID
     })
     useThem(cards)
-  })
+  }, url)
 }
-getThem()
+trelloJSONImport()
